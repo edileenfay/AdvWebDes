d83a3b8ee7f0819bf3e5efc1f7c6795158d4f38a
yaf <105256802+edileenfay@users.noreply.github.com>
2024-01-19 21:12:30 +0800

    Add files via upload

--- resume_javascript.js
+++ resume_javascript.js
@@ -1,1 +1,14 @@
-function randomBackground
+function randomBackground() {
+    var table = document.getElementById("resume");
+    var randomColor = getColor();
+    table.style.backgroundColor = randomColor;
+
+}
+
+function getColor() {
+    var r = Math.floor(Math.random() *256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
